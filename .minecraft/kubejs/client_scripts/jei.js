2b67f3fc4af0142db47231c131ea5b732bbb0738
JackTench <79285604+JackTench@users.noreply.github.com>
2024-05-19 01:58:51 +0100

    Disable lots of stuff from Iron Furnaces.

--- .minecraft/kubejs/client_scripts/jei.js
+++ .minecraft/kubejs/client_scripts/jei.js
@@ -7,7 +7,44 @@ JEIEvents.hideItems(event => {
 
     console.log('jei.js loaded!');
 
+    // Follows format of disable.js
+
     event.hide('twilightforest:uncrafting_table');
+
+    event.hide('ironfurnaces:diamond_furnace');
+    event.hide('ironfurnaces:emerald_furnace');
+    event.hide('ironfurnaces:obsidian_furnace');
+    event.hide('ironfurnaces:crystal_furnace');
+    event.hide('ironfurnaces:netherite_furnace');
+    event.hide('ironfurnaces:silver_furnace');
+    event.hide('ironfurnaces:upgrade_iron');
+    event.hide('ironfurnaces:upgrade_gold');
+    event.hide('ironfurnaces:upgrade_diamond');
+    event.hide('ironfurnaces:upgrade_emerald');
+    event.hide('ironfurnaces:upgrade_obsidian');
+    event.hide('ironfurnaces:upgrade_crystal');
+    event.hide('ironfurnaces:upgrade_netherite');
+    event.hide('ironfurnaces:upgrade_copper');
+    event.hide('ironfurnaces:upgrade_silver');
+    event.hide('ironfurnaces:upgrade_obsidian2');
+    event.hide('ironfurnaces:upgrade_iron2');
+    event.hide('ironfurnaces:upgrade_gold2');
+    event.hide('ironfurnaces:upgrade_silver2');
+    event.hide('ironfurnaces:heater');
+    event.hide('ironfurnaces:item_heater');
+    event.hide('ironfurnaces:augment_blasting');
+    event.hide('ironfurnaces:augment_smoking');
+    event.hide('ironfurnaces:augment_factory');
+    event.hide('ironfurnaces:augment_generator');
+    event.hide('ironfurnaces:augment_speed');
+    event.hide('ironfurnaces:augment_fuel');
+    event.hide('ironfurnaces:item_spooky');
+    event.hide('ironfurnaces:item_xmas');
+    event.hide('ironfurnaces:item_copy');
+    event.hide('ironfurnaces:rainbow_core');
+    event.hide('ironfurnaces:rainbow_plating');
+    event.hide('ironfurnaces:million_furnace');
+    event.hide('ironfurnaces:rainbow_coal');
 
 })
 
--- .minecraft/kubejs/server_scripts/disable.js
+++ .minecraft/kubejs/server_scripts/disable.js
@@ -7,6 +7,43 @@ ServerEvents.recipes(event => {
 
     console.log('disable.js loaded!');
 
+    // Disable Uncrafting Table from Twilight Forest.
     event.remove({output: 'twilightforest:uncrafting_table'});
+
+    // Disable Iron Furnaces junk.
+    event.remove({output: 'ironfurnaces:diamond_furnace'});
+    event.remove({output: 'ironfurnaces:emerald_furnace'});
+    event.remove({output: 'ironfurnaces:obsidian_furnace'});
+    event.remove({output: 'ironfurnaces:crystal_furnace'});
+    event.remove({output: 'ironfurnaces:netherite_furnace'});
+    event.remove({output: 'ironfurnaces:silver_furnace'});
+    event.remove({output: 'ironfurnaces:upgrade_iron'});
+    event.remove({output: 'ironfurnaces:upgrade_gold'});
+    event.remove({output: 'ironfurnaces:upgrade_diamond'});
+    event.remove({output: 'ironfurnaces:upgrade_emerald'});
+    event.remove({output: 'ironfurnaces:upgrade_obsidian'});
+    event.remove({output: 'ironfurnaces:upgrade_crystal'});
+    event.remove({output: 'ironfurnaces:upgrade_netherite'});
+    event.remove({output: 'ironfurnaces:upgrade_copper'});
+    event.remove({output: 'ironfurnaces:upgrade_silver'});
+    event.remove({output: 'ironfurnaces:upgrade_obsidian2'});
+    event.remove({output: 'ironfurnaces:upgrade_iron2'});
+    event.remove({output: 'ironfurnaces:upgrade_gold2'});
+    event.remove({output: 'ironfurnaces:upgrade_silver2'});
+    event.remove({output: 'ironfurnaces:heater'});
+    event.remove({output: 'ironfurnaces:item_heater'});
+    event.remove({output: 'ironfurnaces:augment_blasting'});
+    event.remove({output: 'ironfurnaces:augment_smoking'});
+    event.remove({output: 'ironfurnaces:augment_factory'});
+    event.remove({output: 'ironfurnaces:augment_generator'});
+    event.remove({output: 'ironfurnaces:augment_speed'});
+    event.remove({output: 'ironfurnaces:augment_fuel'});
+    event.remove({output: 'ironfurnaces:item_spooky'});
+    event.remove({output: 'ironfurnaces:item_xmas'});
+    event.remove({output: 'ironfurnaces:item_copy'});
+    event.remove({output: 'ironfurnaces:rainbow_core'});
+    event.remove({output: 'ironfurnaces:rainbow_plating'});
+    event.remove({output: 'ironfurnaces:million_furnace'});
+    event.remove({output: 'ironfurnaces:rainbow_coal'});
 
 })
